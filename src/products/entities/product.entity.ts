a3784b170c78aa10f51f859da38217716534938e
CamiloTello002 <camilojtello5@gmail.com>
2025-05-13 09:20:02 -0500

    feat: define product entity fields
    - id as a uuid
    - title as a UNIQUE text field

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,12 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true, // two products CAN'T have the same name
+  })
+  title: string;
+}
