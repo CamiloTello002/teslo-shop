03f06d05f348ccc96fe7c4fc36f84346da934177
CamiloTello002 <camilojtello5@gmail.com>
2025-05-16 21:04:03 -0500

    feat: add hook before updating entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -48,6 +48,17 @@ export class Product {
     }
     // this will CHECK the slug field. It can come from either
     // the one provided by the user or the title
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '')
+  }
+
+  @BeforeUpdate()
+  checkSlugUpdate() {
+    if (this.title) {
+      this.slug = this.title;
+    }
     this.slug = this.slug
       .toLowerCase()
       .replaceAll(' ', '_')
