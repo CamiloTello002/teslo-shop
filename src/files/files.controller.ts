ba10f3b2d2359702cdd07a191c5ed7385a8a6819
CamiloTello002 <camilojtello5@gmail.com>
2025-05-18 14:52:43 -0500

    feat: implement file absence on controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express'
+import { fileFilter } from './helpers/file-filter.helper';
 
 @Controller('files')
 export class FilesController {
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter
+  }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    if (!file) {
+      throw new BadRequestException('File not found. Make sure the file is an image')
+    }
+    return {
+      file: file.originalname
+    };
   }
 }
 
