d905927e40362230dbeb31185d42ca63e1a98d9b
CamiloTello002 <camilojtello5@gmail.com>
2025-05-18 15:03:38 -0500

    chore: define route for saving product imags

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,17 @@
 import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express'
 import { fileFilter } from './helpers/file-filter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
-    fileFilter
+    fileFilter,
+    storage: diskStorage({
+      destination: './static/products'
+    })
   }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
