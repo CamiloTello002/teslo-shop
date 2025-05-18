005da0ef05c42056caac4425cb2af6730cccc192
CamiloTello002 <camilojtello5@gmail.com>
2025-05-18 17:08:16 -0500

    feat: use service to return image pathname

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,11 +1,19 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express'
 import { fileFilter } from './helpers/file-filter.helper';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/file-namer.helper';
+import { FilesService } from './files.service';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
+  constructor(private readonly filesService: FilesService) { }
+  @Get('product/:imageName')
+  findProductImage(@Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName)
+    return path;
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {
@@ -19,8 +27,9 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('File not found. Make sure the file is an image')
     }
+    const secureUrl = `${file.filename}`
     return {
-      file: file.originalname
+      secureUrl
     };
   }
 }
