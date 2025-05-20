809260499486bd187a5e5b4a2698eb4df720ea37
CamiloTello002 <camilojtello5@gmail.com>
2025-05-20 16:52:29 -0500

    feat: implement initial login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,18 +17,48 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
+      // extracting password for encryption
+      const { password, ...userData } = createUserDto
+
       // user created on memory, not to database
-      const user = this.userRepository.create(createUserDto);
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      });
 
       // this takes the user created on memory and saves it to database
       await this.userRepository.save(user);
 
-      return user;
+      // return everything except the password
+      return { ...user, password: undefined };
+
     } catch (error) {
       console.log(error);
       this.handleDBErrors(error);
     }
   }
+
+
+  async login(loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid');
+    }
+
+    return user
+    // TODO: return JWT
+  }
+
 
   private handleDBErrors(error: any): never {
     if (error.code === '23505') {
