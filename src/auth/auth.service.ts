354a65b02bf12ecc1ec2d11b947b3d3ff63e4777
CamiloTello002 <camilojtello5@gmail.com>
2025-05-20 15:33:19 -0500

    feat: add error handling on duplication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
@@ -22,7 +22,17 @@ export class AuthService {
 
       return user;
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      this.handleDBErrors(error);
     }
+  }
+
+  private handleDBErrors(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail)
+    }
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
