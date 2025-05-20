4bfe037d16f1b9f46623bc61d6d2a1f3c0408e01
CamiloTello002 <camilojtello5@gmail.com>
2025-05-20 15:26:32 -0500

    feat: implement endpoint for registering a user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  async create(createUserDto: CreateUserDto) {
+    try {
+      // user created on memory, not to database
+      const user = this.userRepository.create(createUserDto);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+      // this takes the user created on memory and saves it to database
+      await this.userRepository.save(user);
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+      return user;
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
