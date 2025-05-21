7971bd4aea5a5c903a85af6573a9da21b3b9a89e
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 16:14:57 -0500

    feat: make read operations public (products)
    Only admins can create, update, or delete products.

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,14 +4,14 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles.enum';
 
 @Controller('products')
-// Anyone working with products MUST be authenticated
-@Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
@@ -27,11 +27,13 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(@Param('id', ParseUUIDPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
