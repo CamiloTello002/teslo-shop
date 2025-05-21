7ea2fbd12f0bd51522a1b854e133138813091e97
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 08:25:38 -0500

    feat: setup auth module
    Not only does it import controllers and providers, but also imports
    passport for authentication and sets up configs for jwt operations (such
    as using a secret and setting expiration time)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,14 +3,32 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [AuthController], // for handling http operations
+  providers: [AuthService], // business logic for authentication
+  // user entity for database operations. Mostly used within
+  // auth service
   imports: [
     TypeOrmModule.forFeature([
       User,
-    ])
+    ]),
+    // use passport for authentication strategy
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // configures jwt secret and expiration time
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1h'
+        }
+      })
+    })
   ],
 })
 export class AuthModule { }
