c8514b322fc99997a7ab05f1e13a72cd1268296c
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 15:40:30 -0500

    refactor: expect roles from enum

--- src/auth/decorators/role-protected.decorator.ts
+++ src/auth/decorators/role-protected.decorator.ts
@@ -1,11 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
+import { ValidRoles } from '../interfaces/valid-roles.enum';
 
-// declaring a constant for this prevents typos and mistakes
-// it's a single source of truth, and you can change the name of the metadata if you need it
-// it can be autocompleted on an IDE, which reduces errors and improves dev speed
-// there's more code consistency
 export const META_ROLES = 'roles';
 
-export const RoleProtected = (...args: string[]) => {
+export const RoleProtected = (...args: ValidRoles[]) => {
   return SetMetadata(META_ROLES, args);
 }
