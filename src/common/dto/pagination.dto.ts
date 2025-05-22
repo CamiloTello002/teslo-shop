f12e98ba4bc316abb1cc17937c18a587ce6813e5
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 22:41:57 -0500

    chore: document pagination dto
    This one is VERY important. By documenting this we've already documented
    the query parameters the client can send

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,16 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import { IsNumber, IsOptional, IsPositive, Min } from "class-validator";
 
 export class PaginationDto {
+	@ApiProperty({
+		default: 10,
+		description: 'The amount of rows you need',
+		required: false
+	})
 	@IsNumber()
 	@IsPositive()
-	@Type(() => Number) // this is TRANSFORMING our incoming data
+	@Type(() => Number)
 	@IsOptional()
 	limit?: number;
 
+	@ApiProperty({
+		default: 0,
+		description: 'Amount of rows you want to skip',
+		required: false
+	})
 	@IsNumber()
 	@Min(0)
-	@Type(() => Number) // this is TRANSFORMING our incoming data
+	@Type(() => Number)
 	@IsOptional()
 	offset?: number;
 }
