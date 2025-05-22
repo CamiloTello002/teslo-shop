3ad8416e1fc72467f21c7bec61cc847ddb051a03
CamiloTello002 <camilojtello5@gmail.com>
2025-05-22 11:16:53 -0500

    feat: emit connected clients event
    This will happen whenever a client either connects or disconnects. The
    event is available for EVERYONE

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,18 +1,31 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { Events } from './enums/events.enum';
 
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() webSocketServer: Server;
+
   constructor(private readonly messageWsService: MessageWsService) { }
 
   handleConnection(client: Socket) {
+
     this.messageWsService.registerClient(client);
+
     console.log('Connected clients: ', this.messageWsService.getConnectedClients())
+
+    this.webSocketServer.emit(Events.ClientsUpdated, this.messageWsService.getConnectedClients())
+
   }
 
   handleDisconnect(client: Socket) {
+
     this.messageWsService.removeClient(client.id);
+
     console.log('Connected clients: ', this.messageWsService.getConnectedClients())
+
+    this.webSocketServer.emit(Events.ClientsUpdated, this.messageWsService.getConnectedClients())
   }
 }
