de2f6f426bd5ce54471c59768ab8fe822b8a21e1
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 22:21:50 -0500

    chore: specify properties for documentation

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,48 +1,59 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty()
   @Column('numeric', {
     default: 0
   })
   price: number;
 
+  @ApiProperty()
   @Column({
     type: 'text',
     nullable: true
   })
   description: string;
 
+  @ApiProperty()
   @Column({
     type: 'text',
     unique: true
   })
   slug: string;
 
+  @ApiProperty()
   @Column({
     type: 'integer',
     default: 0
   })
   stock: number;
 
+  @ApiProperty()
   @Column('text', {
     array: true
   })
   sizes: string[]
 
+  @ApiProperty()
   @Column('text')
   gender: string;
 
+  @ApiProperty()
   @Column('text', {
     array: true,
     default: []
