1e283e4d67917df4d1da2e9d21367d1942e77e48
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 22:10:22 -0500

    feat: generate initial documentation on /api

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('Teslo API')
+    .setDescription('RESTful API for Teslo shop application')
+    .setVersion('1.0')
+    .addTag('clothes')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, documentationConfig);
+
+  SwaggerModule.setup('api', app, documentFactory)
+
   const logger = new Logger('Startup')
+
   app.setGlobalPrefix('api');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
     })
   );
+
   await app.listen(process.env.PORT ?? 3000);
+
   logger.log(`App running on port ${process.env.PORT}`)
 }
 bootstrap();
