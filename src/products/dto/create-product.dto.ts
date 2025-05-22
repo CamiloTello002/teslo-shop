b144725b7d44998ac370aa017715745fc69b3208
CamiloTello002 <camilojtello5@gmail.com>
2025-05-21 22:56:22 -0500

    feat: create documentation for create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,42 +1,70 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: "Product's title",
+    example: "Oversized jumper",
+    nullable: false,
+    uniqueItems: true,
+    minLength: 1
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    description: "Product's price",
+    example: 400,
+    nullable: true,
+    default: 0,
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    description: "Product's description",
+    example: "A beautiful jumper you can use on winter",
+    nullable: true,
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description: "Product's slug for SEO. The API will generate it for you in case you don't provide it",
+    example: "oversized_jumper",
+    nullable: true,
+    required: false
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
-  // We can't change this. We're expecting the client to send an 
-  // array of urls, where each url is a string
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
