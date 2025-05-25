9378f8407fb310725f57d11f40997b98c11ab481
CamiloTello002 <camilojtello5@gmail.com>
2025-05-25 11:01:59 -0500

    feat: update logic for registering user
    We'll verify if the user exists and if it exists
    
    And not only will we save the socket, but also the user credentials so
    that we can identify every user

--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Socket } from 'socket.io';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
 
 interface ConnectedClients {
-  [id: string]: Socket
+  [id: string]: {
+    socket: Socket,
+    user: User
+  }
 }
 
 @Injectable()
 export class MessageWsService {
 
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
   private connectedClients: ConnectedClients = {}
 
-  registerClient(client: Socket) {
-    this.connectedClients[client.id] = client;
+  async registerClient(client: Socket, userId: string) {
+    // we need to verify the client
+    const user = await this.userRepository.findOneBy({ id: userId })
+
+    if (!user) throw new Error('User not found')
+
+    if (!user.isActive) throw new Error('User not found')
+
+    this.connectedClients[client.id] = {
+      socket: client,
+      user
+    };
   }
 
   removeClient(clientId: string) {
