98a428ac7054b01657f98edf3b8ea8bbf53ee458
CamiloTello002 <camilojtello5@gmail.com>
2025-05-25 10:40:20 -0500

    feat: import Jwt module

--- src/message-ws/message-ws.module.ts
+++ src/message-ws/message-ws.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MessageWsService } from './message-ws.service';
 import { MessageWsGateway } from './message-ws.gateway';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [MessageWsGateway, MessageWsService],
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '1h'
+        }
+      })
+    }),
+  ]
 })
-export class MessageWsModule {}
+export class MessageWsModule { }
