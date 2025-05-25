9251a6eaf418a635385a27c7ccf4888a43535fbf
CamiloTello002 <camilojtello5@gmail.com>
2025-05-25 10:41:30 -0500

    refactor: import jwt module
    This module already contains the jwt module
    
    Moreover, this module already provides the User entity, which will be
    really useful when I need to perform authentication and authorization.
    That's it.

--- src/message-ws/message-ws.module.ts
+++ src/message-ws/message-ws.module.ts
@@ -3,20 +3,10 @@ import { MessageWsService } from './message-ws.service';
 import { MessageWsGateway } from './message-ws.gateway';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [MessageWsGateway, MessageWsService],
-  imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get('JWT_SECRET'),
-        signOptions: {
-          expiresIn: '1h'
-        }
-      })
-    }),
-  ]
+  imports: [AuthModule]
 })
 export class MessageWsModule { }
